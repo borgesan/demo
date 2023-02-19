aa25f9c9b630604f4f89fb09ee65ba6c6eb8bc40
Andre Borges <andredesouza.borges@gmail.com>
2023-02-19 18:43:50 -0500

    First commit

---
+++ src/main/java/com/example/demo/StringTesting.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class StringTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ola ola ola");
+		
+	}
+
+}
